3c6293215198dfccf147690230698b326ba4479a
jahnaviraj <jahnaviraj25@gmail.com>
2022-08-25 17:31:19 +0530

    show settings for manager, add route

--- desk/src/components/desk/global/CustomIcons.vue
+++ desk/src/components/desk/global/CustomIcons.vue
@@ -230,6 +230,14 @@
 			</clipPath>
 			</defs>
 		</svg>
+		<svg v-if="this.name=='home'" :class="this.class" :width="this.width" :height="this.height" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+			<path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+		</svg>
+		<svg v-if="this.name=='dashboard'" :class="this.class" :width="this.width" :height="this.height" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"/>
+			<path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z"/>
+		</svg>
 		<div v-if="this.name=='company'" :class="this.class">
 			<div v-if="$resources.brandHtml.data" v-html="$resources.brandHtml.data"></div>
 			<div v-else>{{ $resources.helpdeskName.data }}</div>
