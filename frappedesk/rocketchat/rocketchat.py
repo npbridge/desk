144769fc72e1274be7c9577fcd1f27a3e320694d
S N Tiwari <sachtiwari8@gmail.com>
2022-09-02 17:12:50 +0530

    values from env

--- frappedesk/rocketchat/rocketchat.py
+++ frappedesk/rocketchat/rocketchat.py
@@ -1,13 +1,15 @@
 import requests
 import json
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 headers = {
     "content-type": "application/json"
 }
 endPoints = {
-    "sendMessage": os.environ.get('SEND_MESSAGE_END_POINT'),
-    "history": os.environ.get('HISTORY_ENDPOINT')+os.environ.get('ROOM_ID')+"?token="+os.environ.get('TOKEN')
+    "sendMessage": os.getenv('SEND_MESSAGE_END_POINT'),
+    "history": os.getenv('HISTORY_ENDPOINT')+os.getenv('ROOM_ID')+"?token="+os.getenv('TOKEN')
 }
 
 data = {
@@ -22,15 +24,15 @@ params = {
 
 def sendMessages(msg, postData=data, headers=headers):
     postData["msg"] = msg
-    postData["token"] = os.environ.get('TOKEN')
-    postData["rid"] = os.environ.get('ROOM_ID')
+    postData["token"] = os.getenv('TOKEN')
+    postData["rid"] = os.getenv('ROOM_ID')
     res = requests.post(endPoints["sendMessage"], data=json.dumps(postData), headers=headers)
     res = res.json()
     return res['message']['_id']
 
 def getHistory(params=params, headers=headers):
-    params["token"] = os.environ.get('TOKEN')
-    params["rid"] = os.environ.get('ROOM_ID')
+    params["token"] = os.getenv('TOKEN')
+    params["rid"] = os.getenv('ROOM_ID')
     res = requests.get(endPoints["history"], params=params, headers=headers)
     return res.json()
 
